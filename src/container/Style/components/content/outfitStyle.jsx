82de07a0520d1cd458a21149c7d7ffc2359e0c11
richalzackyaflacha <richalzackyi@gmail.com>
2022-05-15 18:19:05 +0700

    Rz OOTD

--- src/container/Style/components/content/outfitStyle.jsx
+++ src/container/Style/components/content/outfitStyle.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 // Add image
 import OutfitPict1 from "../../../../assets/img/NewPhoto2.jpg";
+import OutfitPict2 from "../../../../assets/img/StylePhoto1-1.jpg"
 
 // Add detail
 import DetailOutfit1 from "../../../../assets/outfit/detail-outfit1.png"
+import DetailOutfit2 from "../../../../assets/outfit/detail-outfit2.png"
 
 const OutfitStyle = () => {
     return (
@@ -25,6 +27,24 @@ const OutfitStyle = () => {
 
                 <div className="content-outfit-detail">
                     <img src={DetailOutfit1} alt="Detail" />
+                </div>
+            </div>
+
+
+            <div className="content-outfit">
+                <div className="content-outfit-pict">
+                    <div className="img-btn">
+                        <img src={OutfitPict2} alt="Outfit Style" />
+
+                        <div className="button-to-galery">
+                            <Link to="/galeri">Lihat Foto</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="content-outfit-detail">
+                    <img src={DetailOutfit2} alt="Detail" />
                 </div>
             </div>
 
